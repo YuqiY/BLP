e2ebeac451150056770fec9bf9ce2fafd92b65fb
YuqiY <92695107+YuqiY@users.noreply.github.com>
2024-09-13 14:22:02 -0700

    1.11
    note for what you have done for this update

--- 1.py
+++ 1.py
@@ -1,4 +1,11 @@
 
 import numpy as np 
+from function import cal1,k
+from function2 import cal2
 
-print(12)
+
+
+b = cal1(2.0)
+c = cal2(b)
+print(k)
+print(c)
---
+++ function2.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def cal2(x):
+    return x**2
